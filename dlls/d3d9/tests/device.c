5b7158093781151f145ffbf4fe603fdcedcee0d7
Paul Gofman <gofmanp@gmail.com>
2019-02-25 15:23:33 +0300

    wined3d: Support SWVP mode vertex shaders.

--- dlls/d3d9/tests/device.c
+++ dlls/d3d9/tests/device.c
@@ -7033,15 +7033,11 @@ float4 main(const float4 color : COLOR) : SV_TARGET
 
     vs = NULL;
     hr = IDirect3DDevice9_CreateVertexShader(device, vs_1_256, &vs);
-    todo_wine
     ok(hr == D3D_OK, "Got unexpected hr %#x.\n", hr);
-    if (vs)
-        IDirect3DVertexShader9_Release(vs);
+    IDirect3DVertexShader9_Release(vs);
     hr = IDirect3DDevice9_CreateVertexShader(device, vs_3_256, &vs);
-    todo_wine
     ok(hr == D3D_OK, "Got unexpected hr %#x.\n", hr);
-    if (vs)
-        IDirect3DVertexShader9_Release(vs);
+    IDirect3DVertexShader9_Release(vs);
 
     refcount = IDirect3DDevice9_Release(device);
     ok(!refcount, "Device has %u references left.\n", refcount);
@@ -7058,20 +7054,16 @@ float4 main(const float4 color : COLOR) : SV_TARGET
     ok(hr == D3D_OK, "Got unexpected hr %#x.\n", hr);
 
     hr = IDirect3DDevice9_CreateVertexShader(device, vs_1_256, &vs);
-    todo_wine
     ok(hr == D3D_OK, "Got unexpected hr %#x.\n", hr);
     hr = IDirect3DDevice9_SetVertexShader(device, vs);
     ok(hr == D3D_OK, "Got unexpected hr %#x.\n", hr);
-    if (vs)
-        IDirect3DVertexShader9_Release(vs);
+    IDirect3DVertexShader9_Release(vs);
 
     hr = IDirect3DDevice9_CreateVertexShader(device, vs_3_256, &vs);
-    todo_wine
     ok(hr == D3D_OK, "Got unexpected hr %#x.\n", hr);
     hr = IDirect3DDevice9_SetVertexShader(device, vs);
     ok(hr == D3D_OK, "Got unexpected hr %#x.\n", hr);
-    if (vs)
-        IDirect3DVertexShader9_Release(vs);
+    IDirect3DVertexShader9_Release(vs);
 
 cleanup:
     refcount = IDirect3DDevice9_Release(device);

23bd44e5a7f522374a925cff69699248cefeb4ab
Derek Lesho <dereklesho52@Gmail.com>
2019-06-25 22:37:34 -0400

    winex11.drv: Add support for absolute RawMotion events.
    When running Xwayland, or using pointing devices, the valuators may
    provide absolute values only. In which case, we should translate the
    events to the corresponding absolute rawinput messages.
    
    The server side was trying to convert all rawinput messages to relative
    positions, but it may not be correct as the desktop cursor position may
    not be initialized yet. Keeping the values absolute if we received them
    this way seems more reliable.
    
    Note that we cannot really trust valuator names, as it is possible to
    configure a pointing device to work in relative mode - wacom tablets for
    example - while their axis valuators keep their "Abs X/Y" name. We can
    however use the valuator mode to distinguish between relative movements
    and absolute position events.

--- dlls/user32/message.c
+++ dlls/user32/message.c
@@ -2318,7 +2318,12 @@ static BOOL process_rawinput_message( MSG *msg, const struct hardware_msg_data *
         rawinput->header.hDevice = WINE_MOUSE_HANDLE;
         rawinput->header.wParam  = 0;
 
-        rawinput->data.mouse.usFlags           = MOUSE_MOVE_RELATIVE;
+        if (msg_data->flags & MOUSEEVENTF_ABSOLUTE)
+            rawinput->data.mouse.usFlags = MOUSE_MOVE_ABSOLUTE;
+        else
+            rawinput->data.mouse.usFlags = MOUSE_MOVE_RELATIVE;
+        if (msg_data->flags & MOUSEEVENTF_VIRTUALDESK)
+            rawinput->data.mouse.usFlags |= MOUSE_VIRTUAL_DESKTOP;
         rawinput->data.mouse.u.s.usButtonFlags = 0;
         rawinput->data.mouse.u.s.usButtonData  = 0;
         for (i = 1; i < ARRAY_SIZE(button_flags); ++i)

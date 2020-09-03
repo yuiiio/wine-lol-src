b3d34cc378f1334cc851d24b42739755e56acf5b
Esdras Tarsis <esdrastarsis@gmail.com>
2020-09-02 23:41:19 -0300

    windows.networking.connectivity: Add stub dll.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=46534
    Signed-off-by: Esdras Tarsis <esdrastarsis@gmail.com>

---
+++ dlls/windows.networking.connectivity/windows.networking.connectivity_main.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+
+#define COBJMACROS
+#include "windef.h"
+#include "winbase.h"
+#include "winstring.h"
+#include "wine/debug.h"
+
+#include "objbase.h"
+#include "initguid.h"
+
+#include "activation.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(network);
+
+HRESULT WINAPI DllGetClassObject(REFCLSID clsid, REFIID riid, LPVOID *object)
+{
+    FIXME("clsid %s, riid %s, object %p stub!\n", debugstr_guid(clsid), debugstr_guid(riid), object);
+    return CLASS_E_CLASSNOTAVAILABLE;
+}
+
+HRESULT WINAPI DllGetActivationFactory(HSTRING classid, IActivationFactory **factory)
+{
+    FIXME("classid %s, factory %p stub!\n", debugstr_hstring(classid), factory);
+    return E_NOINTERFACE;
+}

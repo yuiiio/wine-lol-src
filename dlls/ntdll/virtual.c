vitorhnn/wine

ntdll: Force bottom up allocation order for 64 bit arch unless top down is requested.
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=48175
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=46568

dlls/ntdll/virtual.c
@@ -1266,13 +1266,19 @@ static NTSTATUS map_view( struct file_view **view_ret, void *base, size_t size, 
     }
     else
     {
-        size_t view_size = size + mask + 1;
         struct alloc_area alloc;
+        size_t view_size;
 
         alloc.size = size;
         alloc.mask = mask;
         alloc.top_down = top_down;
         alloc.limit = (void*)(get_zero_bits_64_mask( zero_bits_64 ) & (UINT_PTR)user_space_limit);
+
+        if (is_win64 && !top_down)
+        {
+            /* Ditch 0x7ffffe000000 - 0x7fffffff0000 reserved area. */
+            alloc.limit = min(alloc.limit, (void *)0x7ffffe000000);
+        }
 
         if (wine_mmap_enum_reserved_areas( alloc_reserved_area_callback, &alloc, top_down ))
         {
@@ -1283,13 +1289,15 @@ static NTSTATUS map_view( struct file_view **view_ret, void *base, size_t size, 
             goto done;
         }
 
-        if (zero_bits_64)
+        if (is_win64 || zero_bits_64)
         {
             if (!(ptr = map_free_area( address_space_start, alloc.limit, size, mask, top_down, VIRTUAL_GetUnixProt(vprot) )))
                 return STATUS_NO_MEMORY;
             TRACE( "got mem with map_free_area %p-%p\n", ptr, (char *)ptr + size );
             goto done;
         }
+
+        view_size = size + mask + 1;
 
         for (;;)
         {

yuiiio/wine-lol-src

dlls/ntdll/virtual.c
@@ -1314,12 +1314,18 @@ static NTSTATUS map_view( struct file_view **view_ret, void *base, size_t size,
     }
     else
     {
-        size_t view_size = size + granularity_mask + 1;
         struct alloc_area alloc;
+        size_t view_size;
 
         alloc.size = size;
         alloc.top_down = top_down;
         alloc.limit = (void*)(get_zero_bits_64_mask( zero_bits_64 ) & (UINT_PTR)user_space_limit);
+
+        if (is_win64 && !top_down)
+        {
+            /* Ditch 0x7ffffe000000 - 0x7fffffff0000 reserved area. */
+            alloc.limit = min(alloc.limit, (void *)0x7ffffe000000);
+        }
 
         if (unix_funcs->mmap_enum_reserved_areas( alloc_reserved_area_callback, &alloc, top_down ))
         {
@@ -1330,7 +1336,7 @@ static NTSTATUS map_view( struct file_view **view_ret, void *base, size_t size,
             goto done;
         }
 
-        if (zero_bits_64)
+        if (is_win64 || zero_bits_64)
         {
             if (!(ptr = map_free_area( address_space_start, alloc.limit, size,
                                        top_down, VIRTUAL_GetUnixProt(vprot) )))
@@ -1338,6 +1344,8 @@ static NTSTATUS map_view( struct file_view **view_ret, void *base, size_t size,
             TRACE( "got mem with map_free_area %p-%p\n", ptr, (char *)ptr + size );
             goto done;
         }
+
+        view_size = size + granularity_mask + 1;
 
         for (;;)
         {
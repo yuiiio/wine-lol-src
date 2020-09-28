9dec62b091d23a953fbc241fb0cbe42a6162789a
yuiiio <atbjyk@protonmail.com>
2020-09-29 02:23:11 +0900

    Revert "ntdll: Create user shared data section in the server, and initialize it in wineboot."
    This reverts commit e854ea34cc481658ec61f4603d0438e075608c98.

--- dlls/ntdll/loader.c
+++ dlls/ntdll/loader.c
@@ -4404,7 +4404,6 @@ void __wine_process_init(void)
     FILE_umask = umask(0777);
     umask( FILE_umask );
 
-    map_user_shared_data();
     load_global_options();
     version_init();
 
--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -872,6 +872,8 @@ struct init_process_done_request
     mod_handle_t module;
     client_ptr_t ldt_copy;
     client_ptr_t entry;
+    obj_handle_t usd_handle;
+    char __pad_44[4];
 };
 struct init_process_done_reply
 {
@@ -6683,7 +6685,7 @@ union generic_reply
 
 /* ### protocol_version begin ### */
 
-#define SERVER_PROTOCOL_VERSION 606
+#define SERVER_PROTOCOL_VERSION 605
 
 /* ### protocol_version end ### */
 

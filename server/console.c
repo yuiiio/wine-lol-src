b5e31e5d2faa82c739b4a5991ec691d93e02c020
yuiiio <atbjyk@protonmail.com>
2020-09-29 02:23:00 +0900

    Revert "wineconsole: Use IOCTL_CONDRV_GET_INPUT_INFO in WINECON_GetServerConfig."
    This reverts commit eff42369e9c59f330083e25a23762df084ce6869.

--- server/console.c
+++ server/console.c
@@ -1554,10 +1554,7 @@ static int console_ioctl( struct fd *fd, ioctl_code_t code, struct async *async 
                 set_error( STATUS_INVALID_PARAMETER );
                 return 0;
             }
-            info.history_mode  = console->history_mode;
-            info.history_size  = console->history_size;
-            info.edition_mode  = console->edition_mode;
-            info.input_count   = console->recnum;
+            info.input_count = console->recnum;
             return set_reply_data( &info, sizeof(info) ) != NULL;
         }
 

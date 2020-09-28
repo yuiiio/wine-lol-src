c8ee1b8797e60016dabb15852a3e864b660fee36
yuiiio <atbjyk@protonmail.com>
2020-09-29 02:23:03 +0900

    Revert "ntdll: Setup the main thread before initializing ntdll."
    This reverts commit 43be3507c04b56938e985047f2ab55147ed8ddd2.

--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -1507,6 +1507,7 @@ static struct unix_funcs unix_funcs =
     virtual_locked_recvmsg,
     virtual_release_address_space,
     virtual_set_large_address_space,
+    init_threading,
     exit_thread,
     exit_process,
     exec_process,
@@ -1531,29 +1532,6 @@ static struct unix_funcs unix_funcs =
 };
 
 
-/***********************************************************************
- *           start_main_thread
- */
-static void start_main_thread(void)
-{
-    BOOL suspend;
-    TEB *teb = virtual_alloc_first_teb();
-
-    signal_init_threading();
-    signal_alloc_thread( teb );
-    signal_init_thread( teb );
-    dbg_init();
-    server_init_process();
-    startup_info_size = server_init_thread( teb->Peb, &suspend );
-    virtual_map_user_shared_data();
-    virtual_create_builtin_view( ntdll_module );
-    init_cpu_info();
-    init_files();
-    NtCreateKeyedEvent( &keyed_event, GENERIC_READ | GENERIC_WRITE, NULL, 0 );
-    p__wine_set_unix_funcs( NTDLL_UNIXLIB_VERSION, &unix_funcs );
-}
-
-
 #ifdef __APPLE__
 struct apple_stack_info
 {
@@ -1563,7 +1541,7 @@ struct apple_stack_info
 
 static void *apple_wine_thread( void *arg )
 {
-    start_main_thread();
+    p__wine_set_unix_funcs( NTDLL_UNIXLIB_VERSION, &unix_funcs );
     return NULL;
 }
 
@@ -1817,7 +1795,7 @@ void __wine_main( int argc, char *argv[], char *envp[] )
 #ifdef __APPLE__
     apple_main_thread();
 #endif
-    start_main_thread();
+    p__wine_set_unix_funcs( NTDLL_UNIXLIB_VERSION, &unix_funcs );
 }
 
 

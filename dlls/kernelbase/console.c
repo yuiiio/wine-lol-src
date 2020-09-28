80b6614b73c2a6873aa0bc010c413295aa191fcd
yuiiio <atbjyk@protonmail.com>
2020-09-29 02:22:56 +0900

    Revert "kernelbase: Use IOCTL_CONDRV_SET_OUTPUT_INFO in SetConsoleCursorInfo."
    This reverts commit 3b81ea316198e1b128e0e41534da48218f5b641a.

--- dlls/kernelbase/console.c
+++ dlls/kernelbase/console.c
@@ -1181,14 +1181,20 @@ BOOL WINAPI DECLSPEC_HOTPATCH SetConsoleCtrlHandler( PHANDLER_ROUTINE func, BOOL
  */
 BOOL WINAPI DECLSPEC_HOTPATCH SetConsoleCursorInfo( HANDLE handle, CONSOLE_CURSOR_INFO *info )
 {
-    struct condrv_output_info_params params = { SET_CONSOLE_OUTPUT_INFO_CURSOR_GEOM };
+    BOOL ret;
 
     TRACE( "(%p,%d,%d)\n", handle, info->dwSize, info->bVisible);
 
-    params.info.cursor_size    = info->dwSize;
-    params.info.cursor_visible = info->bVisible;
-    return console_ioctl( handle, IOCTL_CONDRV_SET_OUTPUT_INFO, &params, sizeof(params),
-                          NULL, 0, NULL );
+    SERVER_START_REQ( set_console_output_info )
+    {
+        req->handle         = console_handle_unmap( handle );
+        req->cursor_size    = info->dwSize;
+        req->cursor_visible = info->bVisible;
+        req->mask           = SET_CONSOLE_OUTPUT_INFO_CURSOR_GEOM;
+        ret = !wine_server_call_err( req );
+    }
+    SERVER_END_REQ;
+    return ret;
 }
 
 

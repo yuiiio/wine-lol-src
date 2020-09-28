36304e6aa93c09ecd98aefc79dedd1bde36ebd6e
yuiiio <atbjyk@protonmail.com>
2020-09-29 02:22:56 +0900

    Revert "ntdll: Avoid using RtlDosPathNameToNtPathName_U() in the Unix library."
    This reverts commit bcc3410732c60e32eaf3abd808be7482218f57d7.

--- dlls/ntdll/unix/process.c
+++ dlls/ntdll/unix/process.c
@@ -476,10 +476,6 @@ static ULONG get_env_size( const RTL_USER_PROCESS_PARAMETERS *params, char **win
  */
 static int get_unix_curdir( const RTL_USER_PROCESS_PARAMETERS *params )
 {
-    static const WCHAR ntprefixW[] = {'\\','?','?','\\',0};
-    static const WCHAR uncprefixW[] = {'U','N','C','\\',0};
-    const UNICODE_STRING *curdir = &params->CurrentDirectory.DosPath;
-    const WCHAR *dir = curdir->Buffer;
     UNICODE_STRING nt_name;
     OBJECT_ATTRIBUTES attr;
     IO_STATUS_BLOCK io;
@@ -487,27 +483,13 @@ static int get_unix_curdir( const RTL_USER_PROCESS_PARAMETERS *params )
     HANDLE handle;
     int fd = -1;
 
-    if (!(nt_name.Buffer = malloc( curdir->Length + 8 * sizeof(WCHAR) ))) return -1;
-
-    /* simplified version of RtlDosPathNameToNtPathName_U */
-    wcscpy( nt_name.Buffer, ntprefixW );
-    if (dir[0] == '\\' && dir[1] == '\\')
-    {
-        if ((dir[2] == '.' || dir[2] == '?') && dir[3] == '\\') dir += 4;
-        else
-        {
-            wcscat( nt_name.Buffer, uncprefixW );
-            dir += 2;
-        }
-    }
-    wcscat( nt_name.Buffer, dir );
-    nt_name.Length = wcslen( nt_name.Buffer ) * sizeof(WCHAR);
-
+    if (!RtlDosPathNameToNtPathName_U( params->CurrentDirectory.DosPath.Buffer, &nt_name, NULL, NULL ))
+        return -1;
     InitializeObjectAttributes( &attr, &nt_name, OBJ_CASE_INSENSITIVE, 0, NULL );
     status = NtOpenFile( &handle, FILE_TRAVERSE | SYNCHRONIZE, &attr, &io,
                          FILE_SHARE_READ | FILE_SHARE_WRITE,
                          FILE_DIRECTORY_FILE | FILE_SYNCHRONOUS_IO_NONALERT );
-    free( nt_name.Buffer );
+    RtlFreeUnicodeString( &nt_name );
     if (status) return -1;
     server_handle_to_fd( handle, FILE_TRAVERSE, &fd, NULL );
     NtClose( handle );

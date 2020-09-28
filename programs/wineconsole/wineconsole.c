7c6209094fdd7596ad054ba63bc524e163243b95
yuiiio <atbjyk@protonmail.com>
2020-09-29 02:22:52 +0900

    Revert "wineconsole: Use IOCTL_CONDRV_GET_TITLE in WINECON_GetConsoleTitle."
    This reverts commit ffa52880f2519c885ce61b89b847c82e0531c032.

--- programs/wineconsole/wineconsole.c
+++ programs/wineconsole/wineconsole.c
@@ -173,13 +173,22 @@ static void WINECON_SetInsertMode(HANDLE hConIn, unsigned int enable)
  */
 BOOL WINECON_GetConsoleTitle(HANDLE hConIn, WCHAR* buffer, size_t len)
 {
-    DWORD size;
+    BOOL ret;
 
-    if (!DeviceIoControl(hConIn, IOCTL_CONDRV_GET_TITLE, NULL, 0, buffer, len - sizeof(WCHAR), &size, NULL))
-        return FALSE;
+    if (len < sizeof(WCHAR)) return FALSE;
 
-    buffer[size / sizeof(WCHAR)] = 0;
-    return TRUE;
+    SERVER_START_REQ( get_console_input_info )
+    {
+        req->handle = wine_server_obj_handle( hConIn );
+        wine_server_set_reply( req, buffer, len - sizeof(WCHAR) );
+        if ((ret = !wine_server_call_err( req )))
+        {
+            len = wine_server_reply_size( reply );
+            buffer[len / sizeof(WCHAR)] = 0;
+        }
+    }
+    SERVER_END_REQ;
+    return ret;
 }
 
 /******************************************************************

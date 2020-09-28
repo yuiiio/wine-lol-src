8a3a3f18a2bcfbfad87390389d7dab3e1243d67c
yuiiio <atbjyk@protonmail.com>
2020-09-29 02:23:09 +0900

    Revert "ntdll: Use NtCreateThreadEx() for remote thread creation."
    This reverts commit 33c750f50ff8b6f1eae63140e8287c49a5130a60.

--- include/wine/server_protocol.h
+++ include/wine/server_protocol.h
@@ -557,7 +557,7 @@ typedef union
     struct
     {
         enum apc_type    type;
-        unsigned int     flags;
+        int              suspend;
         client_ptr_t     func;
         client_ptr_t     arg;
         mem_size_t       reserve;
@@ -645,7 +645,6 @@ typedef union
     {
         enum apc_type    type;
         unsigned int     status;
-        process_id_t     pid;
         thread_id_t      tid;
         obj_handle_t     handle;
     } create_thread;
@@ -6684,7 +6683,7 @@ union generic_reply
 
 /* ### protocol_version begin ### */
 
-#define SERVER_PROTOCOL_VERSION 608
+#define SERVER_PROTOCOL_VERSION 607
 
 /* ### protocol_version end ### */
 

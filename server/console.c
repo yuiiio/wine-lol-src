86c1760f38facba8e17cf093cccefebff50137cd
yuiiio <atbjyk@protonmail.com>
2020-09-29 02:22:31 +0900

    Revert "kernel32: Use IOCTL_CONDRV_WRITE_CONSOLE in WriteConsoleW when possible."
    This reverts commit ab6056d6774310162ef268ca6af5e3ad2e556a4e.

--- server/console.c
+++ server/console.c
@@ -2106,13 +2106,8 @@ static int screen_buffer_ioctl( struct fd *fd, ioctl_code_t code, struct async *
         }
 
     default:
-        if (!screen_buffer->input || !screen_buffer->input->server || code >> 16 != FILE_DEVICE_CONSOLE)
-        {
-            set_error( STATUS_INVALID_HANDLE );
-            return 0;
-        }
-        return queue_host_ioctl( screen_buffer->input->server, code, screen_buffer->id,
-                                 async, &screen_buffer->ioctl_q );
+        set_error( STATUS_INVALID_HANDLE );
+        return 0;
     }
 }
 

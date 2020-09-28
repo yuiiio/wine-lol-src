19abe174b6a2fb4c8e5c9ccdd50b3d3349b7b882
yuiiio <atbjyk@protonmail.com>
2020-09-29 02:22:53 +0900

    Revert "ntdll: Pass the stack address to the virtual_handle_fault() function."
    This reverts commit 432d504118f4c00cc33c199946b36448cc6355e1.

--- dlls/ntdll/unix/virtual.c
+++ dlls/ntdll/unix/virtual.c
@@ -203,13 +203,6 @@ static struct range_entry *free_ranges;
 static struct range_entry *free_ranges_end;
 
 
-static inline BOOL is_inside_signal_stack( void *ptr )
-{
-    return ((char *)ptr >= (char *)get_signal_stack() &&
-            (char *)ptr < (char *)get_signal_stack() + signal_stack_size);
-}
-
-
 static void reserve_area( void *addr, void *end )
 {
 #ifdef __APPLE__
@@ -2847,15 +2840,16 @@ void virtual_map_user_shared_data(void)
 /***********************************************************************
  *           virtual_handle_fault
  */
-NTSTATUS virtual_handle_fault( void *addr, DWORD err, void *stack )
+NTSTATUS virtual_handle_fault( LPCVOID addr, DWORD err, BOOL on_signal_stack )
 {
     NTSTATUS ret = STATUS_ACCESS_VIOLATION;
-    char *page = ROUND_ADDR( addr, page_mask );
+    void *page = ROUND_ADDR( addr, page_mask );
+    sigset_t sigset;
     BYTE vprot;
 
-    pthread_mutex_lock( &virtual_mutex );  /* no need for signal masking inside signal handler */
+    server_enter_uninterrupted_section( &virtual_mutex, &sigset );
     vprot = get_page_vprot( page );
-    if (!is_inside_signal_stack( stack ) && (vprot & VPROT_GUARD))
+    if (!on_signal_stack && (vprot & VPROT_GUARD))
     {
         set_page_vprot_bits( page, page_size, 0, VPROT_GUARD );
         mprotect_range( page, page_size, 0, 0 );
@@ -2875,7 +2869,7 @@ NTSTATUS virtual_handle_fault( void *addr, DWORD err, void *stack )
                 ret = STATUS_SUCCESS;
         }
     }
-    pthread_mutex_unlock( &virtual_mutex );
+    server_leave_uninterrupted_section( &virtual_mutex, &sigset );
     return ret;
 }
 

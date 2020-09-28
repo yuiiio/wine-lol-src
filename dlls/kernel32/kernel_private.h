dc2eb722728e150cba7a040583df2c90330a3764
yuiiio <atbjyk@protonmail.com>
2020-09-29 02:22:28 +0900

    Revert "kernel32: Remove no longer used bare console support."
    This reverts commit 43f822bca27b983e6ecca11f4204348a38ba7147.

--- dlls/kernel32/kernel_private.h
+++ dlls/kernel32/kernel_private.h
@@ -25,6 +25,7 @@
 
 NTSTATUS WINAPI BaseGetNamedObjectDirectory( HANDLE *dir );
 BOOL           CONSOLE_Init(RTL_USER_PROCESS_PARAMETERS *params) DECLSPEC_HIDDEN;
+BOOL           CONSOLE_Exit(void) DECLSPEC_HIDDEN;
 
 static inline BOOL is_console_handle(HANDLE h)
 {
@@ -48,6 +49,9 @@ static inline BOOL set_ntstatus( NTSTATUS status )
     if (status) SetLastError( RtlNtStatusToDosError( status ));
     return !status;
 }
+
+/* Some Wine specific values for Console inheritance (params->ConsoleHandle) */
+#define KERNEL32_CONSOLE_SHELL          ((HANDLE)2)
 
 extern HMODULE kernel32_handle DECLSPEC_HIDDEN;
 extern SYSTEM_BASIC_INFORMATION system_info DECLSPEC_HIDDEN;

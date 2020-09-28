47656750b590180778da4bf5b75ced1d218dfbab
yuiiio <atbjyk@protonmail.com>
2020-09-29 02:22:34 +0900

    Revert "ntdll: Get rid of the server_init_process_done() Unix library callback."
    This reverts commit 9e973954fdfd2f9e22545bb6311053c835861193.

--- dlls/ntdll/unix/server.c
+++ dlls/ntdll/unix/server.c
@@ -1453,7 +1453,7 @@ void server_init_process(void)
 /***********************************************************************
  *           server_init_process_done
  */
-void server_init_process_done(void)
+void CDECL server_init_process_done( void *relay )
 {
     PEB *peb = NtCurrentTeb()->Peb;
     IMAGE_NT_HEADERS *nt = get_exe_nt_header();

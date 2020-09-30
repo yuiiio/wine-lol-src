0118e3dc62dd9a35a10de103b0afe722f119f3ec
yuiiio <atbjyk@protonmail.com>
2020-09-30 23:43:42 +0900

    origin_3078f10d_fix.mypatch

--- dlls/ntdll/file.c
+++ dlls/ntdll/file.c
@@ -4249,7 +4249,7 @@ NTSTATUS WINAPI NtFlushBuffersFile( HANDLE hFile, IO_STATUS_BLOCK *io )
     if (ret == STATUS_ACCESS_DENIED)
         ret = server_get_unix_fd( hFile, FILE_APPEND_DATA, &fd, &needs_close, &type, NULL );
 
-    if (!ret && (type == FD_TYPE_FILE || type == FD_TYPE_DIR))
+    if (!ret && (type == FD_TYPE_FILE || type == FD_TYPE_DIR || type == FD_TYPE_CHAR))
     {
         if (fsync(fd))
             ret = FILE_GetNtStatus();
